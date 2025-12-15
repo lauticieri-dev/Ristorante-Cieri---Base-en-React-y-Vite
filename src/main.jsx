452a0c13714854cdb0594d65cbd1f0c90c98b877
Lautaro Cieri <lauticieri@gmail.com>
2025-12-14 22:08:28 -0300

    Arreglo de falla Github pages cambio a Github actions

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+console.log('Ristorante Cieri - App iniciando...');
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
